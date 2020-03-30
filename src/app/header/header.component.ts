096e03ca8895da865540737daa4f2ab33f47b503
Muhammad Muhktar Musa <mcube3567@gmail.com>
2020-03-30 14:20:49 -0700

    auth fixed

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs';
-
+import * as fromApp from '../store/app.reducer';
 import { DataStorageService } from '../shared/data-storage.service';
 import { AuthService } from '../auth/auth.service';
+import { Store } from '@ngrx/store';
+import {map} from 'rxjs/operators';
 
 @Component({
   selector: 'app-header',
@@ -14,11 +16,14 @@ export class HeaderComponent implements OnInit, OnDestroy {
 
   constructor(
     private dataStorageService: DataStorageService,
-    private authService: AuthService
+    private authService: AuthService,
+    private store: Store<fromApp.AppState>
   ) {}
 
   ngOnInit() {
-    this.userSub = this.authService.user.subscribe(user => {
+    this.userSub = this.store.select('auth').pipe(map(authState=>{
+   return authState.user
+    })).subscribe(user => {
       this.isAuthenticated = !!user;
       console.log(!!user);
       console.log(!!user);
