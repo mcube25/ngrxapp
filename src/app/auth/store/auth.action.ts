128c2a3819a909c1028d89fc0958033765883d89
Muhammad Muhktar Musa <mcube3567@gmail.com>
2020-04-02 11:05:33 -0700

    further auth effects

--- src/app/auth/store/auth.action.ts
+++ src/app/auth/store/auth.action.ts
@@ -1,16 +1,21 @@
 import { Action } from '@ngrx/store';
 
 export const LOGIN_START ='LOGIN_START'
-export const LOGIN = 'LOGIN';
+export const AUTHENTICATE_SUCCESS = 'LOGIN';
 export const LOGOUT = 'LOGOUT';
-export const LOGIN_FAIL = 'LOGIN_FAIL';
+export const AUTHENTICATE_FAIL = 'LOGIN_FAIL';
+export const SIGNUP_START = 'SIGNUP_START';
+
+
+
+
 
 export class LoginStart implements Action{
 readonly type = LOGIN_START;
 constructor(public payload: {email: string, password: string}){}
 }
-export class Login implements Action {
-   readonly type = LOGIN;
+export class AuthenticateSuccess implements Action {
+   readonly type =  AUTHENTICATE_SUCCESS;
   constructor (public payload: {email: string, userId: string, token: string, expirationDate: Date}){
 
   } 
@@ -18,9 +23,13 @@ export class Login implements Action {
 export class Logout implements Action {
     readonly type = LOGOUT;
 }
-export class LoginFail implements Action {
-    readonly type = LOGIN_FAIL;
+export class AuthenticateFail implements Action {
+    readonly type = AUTHENTICATE_FAIL;
     constructor (public payload: string){}
 }
+export class SignupStart implements Action {
+    readonly type = SIGNUP_START;
+    constructor(public payload: {email: string, password: string} ){}
+}
 
-export type AuthActions = Login | Logout | LoginStart |   LoginFail
+export type AuthActions = AuthenticateSuccess | Logout | LoginStart |   AuthenticateFail | SignupStart
--- src/app/auth/store/auth.reducer.ts
+++ src/app/auth/store/auth.reducer.ts
@@ -16,7 +16,7 @@ const initialState: State = {
 
 export function authReducer(state = initialState, action: AuthActions.AuthActions) {
 switch (action.type) {
-    case AuthActions.LOGIN:
+    case AuthActions.AUTHENTICATE_SUCCESS:
         const user = new User(action.payload.email,
              action.payload.userId,
               action.payload.token,
@@ -40,7 +40,7 @@ switch (action.type) {
                 loading: true
                  
                }
-               case AuthActions.LOGIN_FAIL:
+               case AuthActions.AUTHENTICATE_FAIL:
                    return {
                     ...state,
                     user: null ,
