953ca66608ea521ead9eb83878821cdc2a46aeb7
Muhammad Muhktar Musa <mcube3567@gmail.com>
2020-04-04 09:48:04 -0700

    adding redux debugger

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,10 @@ import { CoreModule } from './core.module';
 import * as fromApp from './store/app.reducer';
 import {EffectsModule} from '@ngrx/effects';
 import { AuthEffects } from './auth/store/auth.effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -22,6 +26,8 @@ import { AuthEffects } from './auth/store/auth.effects';
     AppRoutingModule,
     StoreModule.forRoot(fromApp.appReducer),
      EffectsModule.forRoot([AuthEffects]),
+     StoreDevtoolsModule.instrument({logOnly: environment.production}),
+     StoreRouterConnectingModule.forRoot(),
     SharedModule,
     CoreModule,
     
